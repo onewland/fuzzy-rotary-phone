87d6b590c527fc516faed4b32c0bc789c78d14a8
Oliver Newland <oliver.newland@crowdflower.com>
2016-08-01 15:14:37 -0700

    happy path test of Match model

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,7 +1,7 @@
 class Match < ActiveRecord::Base
   validates_presence_of :channel, :x_player, :o_player
 
-  validate :only_open_match_or_challenge_in_channel
+  validate :only_open_match_or_challenge_in_channel?
 
   def self.challenge(channel:, x_player:, o_player:)
     Match.create(channel: channel,
@@ -10,10 +10,22 @@ class Match < ActiveRecord::Base
                  status: 'challenge_open')
   end
 
-  def only_open_match_or_challenge_in_channel
+  def accept_challenge(o_player)
+    if self.o_player == o_player && status == 'challenge_open'
+      self.status = 'game_in_progress'
+      self.accepted_at = Time.now
+      self.board = "........."
+      save
+      return self
+    else
+      errors.add(:o_player, :not_challenged, "#{o_player} was not challenged to a match")
+    end
+  end
+
+  def only_open_match_or_challenge_in_channel?
     matches = Match.where(channel: channel, status: ['game_in_progress','challenge_open']).load
     unless matches.count == 0 || (id == matches.first.id)
-      errors.add(:channel, "channel #{channel} has outstanding challenge or match")
+      errors.add(:channel, :game_running, "channel #{channel} has outstanding challenge or match")
     end
   end
 end
