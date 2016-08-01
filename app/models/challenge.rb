fb2bbe2cdd824b7b19d324a2fbf5191f09cfe55a
Oliver Newland <oliver.newland@crowdflower.com>
2016-08-01 15:45:58 -0700

    commit Challenge model to SCM

---
+++ app/models/challenge.rb
@@ -0,0 +1,18 @@
+class Challenge
+  def self.create(channel:, x_player:, o_player:)
+    Match.challenge(channel: channel, x_player: x_player, o_player: o_player)
+  end
+
+  def self.accept_challenge(channel:, o_player:)
+    match = Match.where(channel: channel, status: 'challenge_open')
+    match.accept(o_player)
+  end
+
+  def self.challenge_params(params)
+    {
+      channel: params[:channel_name],
+      x_player: params[:user_name],
+      o_player: params[:text]
+    }
+  end
+end
