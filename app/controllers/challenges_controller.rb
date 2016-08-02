60ead6c11d2141361cc837372050551181413818
Oliver Newland <oliver.newland@crowdflower.com>
2016-08-01 17:33:57 -0700

    fix accept message

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -17,8 +17,13 @@ class ChallengesController < ApplicationController
     channel = params[:channel_name]
     challenge = Challenge.accept_challenge(channel: channel, o_player: o_player)
     if challenge.errors.empty?
-
+      render json: {
+        response_type: "in_channel",
+        text: "@#{challenge.o_player} has accepted @#{challenge.x_player}'s tic-tac-toe challenge.
+               To accept, run /ttt-accept"
+      }
     else
+      render json: { text: challenge.errors.full_messages.join("\n") }
     end
   end
 end
