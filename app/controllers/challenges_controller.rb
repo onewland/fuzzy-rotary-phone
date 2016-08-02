6e5eb2ee6a2480fee69f22576a2c13bd4f5f2667
Oliver Newland <oliver.newland@crowdflower.com>
2016-08-01 17:26:47 -0700

    syntax error

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -3,7 +3,7 @@ class ChallengesController < ApplicationController
     challenge = Challenge.create(Challenge.challenge_params(params))
     if challenge.errors.empty?
       render json: {
-        response_type: "in_channel"
+        response_type: "in_channel",
         text: "@#{challenge.o_player}, @#{challenge.x_player} has challenged you to a game.
                To accept, run /ttt-accept"
       }
@@ -13,5 +13,12 @@ class ChallengesController < ApplicationController
   end
 
   def accept
+    o_player = params[:user_name]
+    channel = params[:channel_name]
+    challenge = Challenge.accept_challenge(channel: channel, o_player: o_player)
+    if challenge.errors.empty?
+
+    else
+    end
   end
 end
