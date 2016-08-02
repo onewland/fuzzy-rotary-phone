b07215172bc7641779afc138719be66d7dd7d9eb
Oliver Newland <oliver.newland@crowdflower.com>
2016-08-01 18:15:47 -0700

    add board model

---
+++ app/models/board.rb
@@ -0,0 +1,31 @@
+class Board
+  EMPTY_CHAR = "."
+  ROW_SEP = "\n`" + ("-" * 10) + "`\n"
+  attr_accessor :contents
+
+  def initialize(contents)
+    @contents = contents
+  end
+
+  def self.from_descriptor(str)
+    Board.new(str.to_a)
+  end
+
+  def display
+    display_rows = []
+    contents.each_slice(3) do |c_row|
+      display_rows <<  c_row.map { |c| display_cell(c) }
+    end
+    display_rows.map { |r|
+      "`" + r.join(" | ") + "`"
+    }.join(ROW_SEP)
+  end
+
+  def display_cell(c)
+    if c == EMPTY_CHAR
+      " "
+    else
+      c
+    end
+  end
+end
