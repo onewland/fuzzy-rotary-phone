e90df46c4df1d4aa547d196c911c43d0499cc77c
Oliver Newland <oliver.newland@crowdflower.com>
2016-08-01 17:24:09 -0700

    make challenge in-channel

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -2,7 +2,11 @@ class ChallengesController < ApplicationController
   def create
     challenge = Challenge.create(Challenge.challenge_params(params))
     if challenge.errors.empty?
-      render json: { text: "Successfully challenged #{challenge.o_player} to a game" }
+      render json: {
+        response_type: "in_channel"
+        text: "@#{challenge.o_player}, @#{challenge.x_player} has challenged you to a game.
+               To accept, run /ttt-accept"
+      }
     else
       render json: { text: challenge.errors.full_messages.join("\n") }
     end
