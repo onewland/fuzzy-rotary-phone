a41cd2989b2c2443f1e5a7154d6ab81d0312c026
Oliver Newland <oliver.newland@crowdflower.com>
2016-08-05 13:20:05 -0700

    clean up game abort responses

--- app/models/commands/abort.rb
+++ app/models/commands/abort.rb
@@ -5,8 +5,13 @@ module Commands
         status: ['game_in_progress','challenge_open'],
         channel: @ctx[:channel_name]
       ).first
-      @match.abort_game
-      ::CommandResponse.new(text: "ended game/challenge of #{@match.x_player} vs. #{@match.o_player}")
+      
+      if @match
+        @match.abort_game
+        ::CommandResponse.new(text: "Ended game/challenge of #{@match.x_player} vs. #{@match.o_player}")
+      else
+        ::CommandResponse.new(text: "No game in this channel to abort")
+      end
     end
   end
 end
