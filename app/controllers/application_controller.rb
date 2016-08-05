933a82b042b9e61984dc3f3d806be01356c82a8e
Oliver Newland <oliver.newland@crowdflower.com>
2016-08-05 15:50:47 -0700

    return 500s as json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  rescue_from StandardError do |e|
+    render json: { text: "There was an error processing your request"}, status: 500
+  end
 end
