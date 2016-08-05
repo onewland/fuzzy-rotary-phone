0c7cb970c502a581090a3764a13a09e9a8209176
Oliver Newland <oliver.newland@crowdflower.com>
2016-08-04 22:49:30 -0700

    fix handle of accept non-existent challenge

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,10 +1,12 @@
 class Challenge
+  class ChallengeNotFound < StandardError; end
   def self.create(channel:, x_player:, o_player:)
     Match.challenge(channel: channel, x_player: x_player, o_player: o_player)
   end
 
   def self.accept_challenge(channel:, o_player:)
     match = Match.where(channel: channel, status: 'challenge_open').first
+    return ChallengeNotFound unless match
     match.accept_challenge(o_player)
   end
 
