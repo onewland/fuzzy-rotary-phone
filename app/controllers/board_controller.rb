0b6dceea3682b8ef3f072347ceb7dbfc497ac621
Oliver Newland <oliver.newland@crowdflower.com>
2016-08-05 13:36:26 -0700

    remove non-multiplexed routes for accept challenge/board move

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -15,41 +15,6 @@ class BoardController < ApplicationController
     end
   end
 
-  def move
-    player_attempt_move = params[:user_name]
-    position = params[:text].to_i
-
-    begin
-      @match.user_attempt_move(username: player_attempt_move, position: position)
-
-      channel_output = "#{player_attempt_move} made their move.\n"
-      channel_output << @match.board_inst.display
-      channel_output << "\n"
-      if @match.declaration
-        channel_output << @match.declaration
-      else
-        channel_output << "It is now #{@match.current_user_name}'s turn\n"
-      end
-
-      render json: {
-        response_type: 'in_channel',
-        text: channel_output
-      }
-    rescue Board::BoardSpaceTaken
-      render json: {
-        text: 'That space is already taken.'
-      }
-    rescue Match::TurnOutOfOrder
-      render json: {
-        text: 'Sorry, but it\'s not currently your turn.'
-      }
-    rescue Match::PlayerNotInMatch
-      render json: {
-        text: 'You are not part of the current match in this channel.'
-      }
-    end
-  end
-
   def get_match
     @match = Match.where(
       status: 'game_in_progress',
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   post '/ping' => 'ping#show'
   get '/ping' => 'ping#show'
 
-  post '/challenges/accept' => 'challenges#accept'
-
   get '/board' => 'board#show'
-  post '/board/move' => 'board#move'
 
   post '/ttt/ingest' => 'ttt#ingest'
 end
