bf1a5f8e6dd2dc3204127f490e0f7566508bbe20
Oliver Newland <oliver.newland@crowdflower.com>
2016-08-05 13:02:06 -0700

    remove non-multiplexed create challenge

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,18 +1,4 @@
 class ChallengesController < ApplicationController
-  def create
-    challenge = Challenge.create(Challenge.challenge_params(params))
-
-    if challenge.errors.empty?
-      render json: {
-        response_type: "in_channel",
-        text: "@#{challenge.o_player}, @#{challenge.x_player} has challenged you to a game.
-               To accept, run /ttt-accept"
-      }
-    else
-      render json: { text: challenge.errors.full_messages.join("\n") }
-    end
-  end
-
   def accept
     o_player = params[:user_name]
     channel = params[:channel_name]
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   post '/ping' => 'ping#show'
   get '/ping' => 'ping#show'
 
-  post '/challenges' => 'challenges#create'
   post '/challenges/accept' => 'challenges#accept'
 
   get '/board' => 'board#show'
