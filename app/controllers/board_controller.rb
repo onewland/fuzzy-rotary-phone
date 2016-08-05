22f21f62e32e442f1ee0280f70ae953b47b3685e
Oliver Newland <oliver.newland@crowdflower.com>
2016-08-04 22:09:04 -0700

    handle edge cases more gracefully

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -19,21 +19,35 @@ class BoardController < ApplicationController
     player_attempt_move = params[:user_name]
     position = params[:text].to_i
 
-    @match.user_attempt_move(username: player_attempt_move, position: position)
+    begin
+      @match.user_attempt_move(username: player_attempt_move, position: position)
 
-    channel_output = "#{player_attempt_move} made their move.\n"
-    channel_output << @match.board_inst.display
-    channel_output << "\n"
-    if @match.declaration
-      channel_output << @match.declaration
-    else
-      channel_output << "It is now #{@match.current_user_name}'s turn\n"
+      channel_output = "#{player_attempt_move} made their move.\n"
+      channel_output << @match.board_inst.display
+      channel_output << "\n"
+      if @match.declaration
+        channel_output << @match.declaration
+      else
+        channel_output << "It is now #{@match.current_user_name}'s turn\n"
+      end
+
+      render json: {
+        response_type: 'in_channel',
+        text: channel_output
+      }
+    rescue Board::BoardSpaceTaken
+      render json: {
+        text: 'That space is already taken.'
+      }
+    rescue Match::TurnOutOfOrder
+      render json: {
+        text: 'Sorry, but it\'s not currently your turn.'
+      }
+    rescue Match::PlayerNotInMatch
+      render json: {
+        text: 'You are not part of the current match in this channel.'
+      }
     end
-
-    render json: {
-      response_type: 'in_channel',
-      text: channel_output
-    }
   end
 
   def get_match
